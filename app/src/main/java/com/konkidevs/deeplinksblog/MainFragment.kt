60aa5f7af4f2c18b99f32bef600d4cefcc56ec17
AchsKonkiDevs <alejandrocerrillo@konkidevs.com>
2019-04-21 12:39:31 -0500

    first commit

---
+++ app/src/main/java/com/konkidevs/deeplinksblog/MainFragment.kt
@@ -0,0 +1,30 @@
+package com.konkidevs.deeplinksblog
+
+
+import android.os.Bundle
+import android.support.v4.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
+import kotlinx.android.synthetic.main.fragment_main.*
+
+
+class MainFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? = inflater.inflate(R.layout.fragment_main, container, false)
+
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        navigateButton.setOnClickListener {
+            findNavController().navigate(R.id.action_mainFragment_to_detailFragment)
+        }
+
+    }
+
+}
